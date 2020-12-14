0d419665072e8774a9e15e5b4ad7f54e9e9b82b2
Lamalle <julienlamalle63100@gmail.com>
2020-12-14 17:00:12 +0100

    updated posts table with array on pictures url columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_151928) do
+ActiveRecord::Schema.define(version: 2020_12_14_155755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 2020_12_14_151928) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "likes", default: 0
+    t.string "pictures_url", default: [], array: true
     t.index ["garden_id"], name: "index_posts_on_garden_id"
   end
 
