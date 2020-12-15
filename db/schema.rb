fe77711ffa60bcb2d6fb1ee3fc5876cc7b809c6f
Lamalle <julienlamalle63100@gmail.com>
2020-12-15 10:12:17 +0100

    added migration for end_date on events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_155755) do
+ActiveRecord::Schema.define(version: 2020_12_15_090921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_12_14_155755) do
     t.bigint "garden_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "end_date"
     t.index ["garden_id"], name: "index_events_on_garden_id"
   end
 
